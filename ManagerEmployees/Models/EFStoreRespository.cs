45122c229029a528194dd090d9bb9066a4848c34
nta228 <64982003+nta228@users.noreply.github.com>
2022-02-24 10:38:08 +0700

    Add files via upload

---
+++ ManagerEmployees/Models/EFStoreRespository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRM_Hieu_iceTea.Models
+{
+    public class EFStoreRespository:IStoreRepository
+    {
+        private StoreDbContext context;
+        public EFStoreRespository(StoreDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Employees> Employees => context.Employees;
+    }
+}
---
+++ ManagerEmployees/Models/IStoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ManagerEmployees.Models
+{
+    public interface IStoreRepository
+    {
+        IQueryable<Employees> Employees { get; }
+    }
+}
