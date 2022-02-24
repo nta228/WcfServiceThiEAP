8e509135c51a79cf2057f443c99a480b90d0937c
nta228 <64982003+nta228@users.noreply.github.com>
2022-02-24 10:28:23 +0700

    Add files via upload

---
+++ ManagerEmployees/Models/IStoreRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HRM_Hieu_iceTea.Models
+{
+    public interface IStoreRepository
+    {
+        IQueryable<Employees> Employees { get; }
+    }
+}
